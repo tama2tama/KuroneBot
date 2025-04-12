5c8ca0c0fa9af89780c1a8250e67e76c1f15722d
Glitch (minipotato-bot-by-eoi) <none>
2025-04-12 09:13:05 +0000

    ✊🙇 Checkpoint ./handlers/messageCreate.mjs:76225042/692

--- handlers/messageCreate.mjs
+++ handlers/messageCreate.mjs
@@ -5,6 +5,10 @@ export default async(message) => {
     await message.react("🥔");
   }
   
+  if (message.content.match(/help/)) {
+    await message.reply("コマンド一覧です。\n　コマンド1：　\n 　コマンド2：");
+  }
+  
   if (message.content.match(/にゃん|にゃーん|にゃ～ん/)) {
     await message.reply("にゃ～ん");
   }
